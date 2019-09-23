6161cc74c96232911a22a605156ab6277350061b
blackdiz <blackdiz@gmail.com>
2019-09-23 16:03:24 +0800

    implement link

--- src/main/java/chapter5/LinkList.java
+++ src/main/java/chapter5/LinkList.java
@@ -1,5 +1,21 @@
 package chapter5;
 
+class Link {
+
+  public int iData;
+  public int dData;
+  public Link next;
+
+  public Link(int iData, int dData) {
+    this.iData = iData;
+    this.dData = dData;
+  }
+
+  public void displayLink() {
+    System.out.print("{" + iData + ", " + dData + "}");
+  }
+}
+
 public class LinkList {
 
 }
