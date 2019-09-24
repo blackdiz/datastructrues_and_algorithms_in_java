29ca96bcb8910cac44c5add39c2d098a3aa5c679
blackdiz <blackdiz@gmail.com>
2019-09-24 10:17:45 +0800

    simple linked list implementation

--- src/main/java/chapter5/LinkList.java
+++ src/main/java/chapter5/LinkList.java
@@ -3,10 +3,10 @@ package chapter5;
 class Link {
 
   public int iData;
-  public int dData;
+  public double dData;
   public Link next;
 
-  public Link(int iData, int dData) {
+  public Link(int iData, double dData) {
     this.iData = iData;
     this.dData = dData;
   }
@@ -18,4 +18,59 @@ class Link {
 
 public class LinkList {
 
+  private Link first;
+
+  public LinkList() {
+    first = null;
+  }
+
+  public boolean isEmpty() {
+    return first == null;
+  }
+
+  public void insertFisrt(int iData, double dData) {
+    Link newLink = new Link(iData, dData);
+    newLink.next = first; // 將下個link的參照指到目前的第一個link
+    first = newLink; // 新的link變成第一個link
+  }
+
+  public Link deleteFirst() {
+    if (isEmpty()) {
+      return null;
+    }
+
+    Link temp = first;
+    // 將第一個link的參照指向目前第一個link.next, 所以目前第一個link就不再能被讀到
+    first = first.next;
+
+    return temp;
+  }
+
+  public void displayList() {
+    System.out.println("List (first --> last): ");
+    Link currentLink = first;
+    while (currentLink != null) {
+      currentLink.displayLink();
+      currentLink = currentLink.next;
+    }
+    System.out.println("");
+  }
+
+  public static void main(String[] args) {
+    LinkList list = new LinkList();
+
+    list.insertFisrt(22, 2.99);
+    list.insertFisrt(44, 4.99);
+    list.insertFisrt(66, 6.99);
+    list.insertFisrt(88, 8.99);
+
+    list.displayList();
+    while (!list.isEmpty()) {
+      Link deletedLink = list.deleteFirst();
+      System.out.print("Deleted ");
+      deletedLink.displayLink();
+      System.out.println("");
+    }
+    list.displayList();
+  }
 }
