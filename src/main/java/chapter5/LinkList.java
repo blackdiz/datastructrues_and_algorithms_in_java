41481137166857a1910fe615918f1a4270510b72
blackdiz <blackdiz@gmail.com>
2019-09-24 10:34:57 +0800

    move the Link class back to the LinkList class

--- src/main/java/chapter5/LinkList.java
+++ src/main/java/chapter5/LinkList.java
@@ -1,5 +1,21 @@
 package chapter5;
 
+class Link {
+
+  public int iData;
+  public double dData;
+  public Link next;
+
+  public Link(int iData, double dData) {
+    this.iData = iData;
+    this.dData = dData;
+  }
+
+  public void displayLink() {
+    System.out.print("{" + iData + ", " + dData + "}");
+  }
+}
+
 public class LinkList {
 
   private Link first;
