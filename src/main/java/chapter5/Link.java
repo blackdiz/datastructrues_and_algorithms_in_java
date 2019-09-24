5516e8d85da7ce56306423e95685b053cec790f4
blackdiz <blackdiz@gmail.com>
2019-09-24 10:32:02 +0800

    isolate the Link class from the LinkList class

---
+++ src/main/java/chapter5/Link.java
@@ -0,0 +1,17 @@
+package chapter5;
+
+class Link {
+
+  public int iData;
+  public double dData;
+  public Link next;
+
+  public Link(int iData, double dData) {
+    this.iData = iData;
+    this.dData = dData;
+  }
+
+  public void displayLink() {
+    System.out.print("{" + iData + ", " + dData + "}");
+  }
+}
