33aa0af2a6409d04a52c6f576182691709d9df98
blackdiz <blackdiz@gmail.com>
2019-10-28 11:47:42 +0800

    add node

--- src/main/java/chapter8/redblacktree/RedBlackTree.java
+++ src/main/java/chapter8/redblacktree/RedBlackTree.java
@@ -1,5 +1,21 @@
 package chapter8.redblacktree;
 
+class Node {
+
+  public int key;
+  public double value;
+  private boolean isRed = true; // 預設新建的 node 都是紅色
+
+  public void changeColor() {
+    isRed = !isRed;
+  }
+
+  public void display() {
+    System.out.print("{" + key + ", " + value + ", color: " + (isRed ? "red" : "black"));
+  }
+}
+
 public class RedBlackTree {
 
+  private Node root;
 }
