f142817e83012ccab1de9fe24ab632b6ba77ecb6
blackdiz <blackdiz@gmail.com>
2019-11-01 16:41:45 +0800

    implementing quadratic probe hash table

---
+++ src/main/java/chapter11/quadraticprobe/QuadraticProbeHashTable.java
@@ -0,0 +1,46 @@
+package chapter11.quadraticprobe;
+
+class DataItem {
+
+  private int iData;
+
+  public DataItem(int data) {
+    iData = data;
+  }
+
+  public int getKey() {
+    return iData;
+  }
+}
+
+public class QuadraticProbeHashTable {
+
+  private int arraySize;
+  private DataItem[] hashArray;
+  private DataItem nonItem;
+
+  public QuadraticProbeHashTable(int size) {
+    arraySize = size;
+    hashArray = new DataItem[arraySize];
+    nonItem = new DataItem(-1);
+  }
+
+  public void insert(DataItem newItem) {
+    int hashValue = hash(newItem.getKey());
+
+    int step = 0;
+    int probeLength = hashValue;
+    while (hashArray[probeLength] != null
+        && hashArray[probeLength].getKey() != -1) {
+      step++;
+      probeLength = hashValue + (step * step);
+      probeLength %= arraySize;
+    }
+
+    hashArray[hashValue] = newItem;
+  }
+
+  private int hash(int key) {
+    return key % arraySize;
+  }
+}
