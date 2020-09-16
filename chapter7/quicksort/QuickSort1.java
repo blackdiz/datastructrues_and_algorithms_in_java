b3d41c1d450f5ecc96a1c67733f373a210c6f921
Luke <blackdiz@gmail.com>
2020-09-16 23:41:56 +0800

    Add more explanation at partition method

--- chapter7/quicksort/QuickSort1.java
+++ chapter7/quicksort/QuickSort1.java
@@ -35,23 +35,27 @@ class Array {
       long pivot = array[right];
 
       int pivotIndex = partitionIt(left, right, pivot);
-      // 因為pivot已經在最終排序的正確位置, 因此不用加入下次quicksort
+      // 因為 pivot 已經在最終排序的正確位置, 因此不用加入下次 quicksort
       recQuickSort(left, pivotIndex - 1);
       recQuickSort(pivotIndex + 1, right);
     }
   }
 
   private int partitionIt(int left, int right, long pivot) {
-    // leftPtr指向從左邊開始找到大於pivot時的index
+    // leftPtr 指向從左邊開始找到大於 pivot 時的 index, 用 left - 1 是因為等一下會用 ++leftPar 取 array
+    // 中值來和 pivot 比較
     int leftPtr = left - 1;
-    // rightPtr指向從右邊開始找到小於pivot時的index, 因為最右邊為pivot本身, 所以由right開始,
+    // rightPtr 指向從右邊開始找到小於 pivot 時的 index, 因為最右邊為 pivot 本身, 所以由 right 開始,
     // 每次取值比較時先-1再取值
     int rightPtr = right;
 
     while (true) {
       // 找到 > pivot的值時結束迴圈
-      // 和Partition.java裡不同，這裡不用加上left < right的判斷, 因為最右邊本身就是pivot
-      // 所以array[++leftPtr]到最右邊時必定 == pivot 而不會滿足 < pivot
+      // 和 Partition.java 裡不同，這裡不用加上 left < right 的判斷, 因為最右邊本身就是 pivot
+      // 所以 array[++leftPtr] 到最右邊時必定 == pivot 而不會滿足 < pivot,
+      // 用 ++leftPtr 是因為如果用 leftPar++, 則如果 array 中所有元素 < pivot 時, 
+      // 最終 leftPtr 的位置會在 array 長度 + 1, 而我們最後會交換 leftPtr 和 pivot 的位置,
+      // 這樣會造成 index of bounds
       while (array[++leftPtr] < pivot) {
 
       }
@@ -60,7 +64,7 @@ class Array {
 
       }
 
-      // 當leftPtr和rightPtr交錯時表示陣列已經遍歷比較完, 因此結束
+      // 當 leftPtr 和 rightPtr 交錯時表示陣列已經遍歷比較完, 因此結束
       if (leftPtr >= rightPtr) {
         break;
       } else {
@@ -68,8 +72,8 @@ class Array {
       }
     }
 
-    // 最後leftPtr指向的位置會是右邊所有元素都 > pivot的子陣列的最左邊, 因此將它和最右邊pivot的位置
-    // 互換, 這樣pivot就會移到最終排序時的正確位置
+    // 最後 leftPtr 指向的位置會是右邊所有元素都 > pivot 的子陣列的最左邊, 因此將它和最右邊 pivot 的位置
+    // 互換, 這樣 pivot 就會移到最終排序時的正確位置
     swap(leftPtr, right);
 
     return leftPtr;
